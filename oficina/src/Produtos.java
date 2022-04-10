1d6671dd92ab15cd1647b0044844d764fecf80e0
matheus charles ramos <98620399+Charlles21@users.noreply.github.com>
2022-04-10 20:23:49 -0300

    Update Produtos.java

--- oficina/src/Produtos.java
+++ oficina/src/Produtos.java
@@ -1,62 +1,84 @@
+import Funcoes.funcao;
 
+public class Produtos implements funcao {
+    private int Codigo;
+    private String Nome;  
+    private int Estoque;
+    private int Quantidade;
+    private double Valor; 
 
-public class Produtos { 
+    
 
-    private String NomeProduto;
-    private Double ValorProduto;
-    private int QunatidadeEstoque;
-    private int CodigoProduto;  
-
-    public void Produto(){
-
-    }
-    public void Produto(String NomeProduto,Double ValorProduto,int QunatidadeEstoque,int CodigoProduto){
-        this.NomeProduto = NomeProduto;
-        this.ValorProduto = ValorProduto;
-        this.QunatidadeEstoque = QunatidadeEstoque;
-        this.CodigoProduto = CodigoProduto;
-    }    
-
-    public String getNomeProduto() {
-        return NomeProduto;
-    }
-    public void setNomeProduto(String nomeProduto) {
-        NomeProduto = nomeProduto;
-    }
-    public Double getValorProduto() {
-        return ValorProduto;
-    }
-    public void setValorProduto(Double valorProduto) {
-        ValorProduto = valorProduto;
-    }
-    public int getQunatidadeEstoque() {
-        return QunatidadeEstoque;
-    }
-    public void setQunatidadeEstoque(int qunatidadeEstoque) {
-        QunatidadeEstoque = qunatidadeEstoque;
-    }
-    public int getCodigoProduto() {
-        return CodigoProduto;
-    }
-    public void setCodigoProduto(int codigoProduto) {
-                CodigoProduto = codigoProduto;
-    }
-   
-
-    @Override
-    public String toString() {
-        return "Produtos [CodigoProduto=" + CodigoProduto + ", Lista=" + ", NomeProduto=" + NomeProduto
-                + ", QunatidadeEstoque=" + QunatidadeEstoque + ", ValorProduto=" + ValorProduto + "]";
-    }
-    public static Iterable<Produtos> stream() {
-        return null;
+    public Produtos(int codigo, String nome, double Valor, int Quantidade, int Estoque){
+        this.Codigo = codigo;
+        this.Nome = nome;
+        this.Valor = Valor;   
+        this.Quantidade = Quantidade;
+        this.Estoque = Estoque;    
     }
 
+    public Produtos(){
+
+    } 
 
 
     
 
- 
-}       
+    public int getQuantidade() {
+        return Quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        Quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return Valor;
+    }
+
+    public void setValor(double valor) {
+        Valor = valor;
+    }
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        Codigo = codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
 
 
+    public int getEstoque() {
+        return Estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        Estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome: " + Nome + "\nCodigo: " + Codigo + "\nEstoque: " + Estoque + "\nValor: "+Valor+ "\n";
+    }
+
+    public String Relatorio(){
+        return "\nNome: " + Nome + "\nCodigo: " + Codigo + "\nEstoque: " + Estoque + "\nValor: "+Valor+ "\nQuantidade Ja vendida: "+ Quantidade +"\n";
+    }
+
+    @Override
+    public Object venda(double Valor, int Quantidade) {  
+        Valor = Valor * Quantidade;            
+        return Valor;
+    }
+   
+    
+}
